60224a250b1daab1a15f2a8a2610e64097977222
Renreal <renreal.dev@gmail.com>
2025-09-20 13:11:44 +0800

    update env creds

--- config/db.js
+++ config/db.js
@@ -1,20 +1,29 @@
-//   Database, environment configs
+// //   Database, environment configs
+
+// const { Pool } = require("pg");
+// require("dotenv").config();
+
+// const pool = new Pool({
+//   user: process.env.DB_USER,
+//   host: process.env.DB_HOST,
+//   database: process.env.DB_NAME,
+//   password: process.env.DB_PASSWORD,
+//   port: process.env.DB_PORT,
+//    ssl: {
+//     rejectUnauthorized: false, // required for Supabase
+//   },
+// });
+
+// module.exports = pool;
+
 
 const { Pool } = require("pg");
-require("dotenv").config();
 
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
-   ssl: {
-    rejectUnauthorized: false, // required for Supabase
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
   },
 });
 
 module.exports = pool;
-
-
-
